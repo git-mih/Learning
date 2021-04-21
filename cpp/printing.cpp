082da0b99f2cf523710cd0a3707c540391333553
git-mih <git.mih@gmail.com>
2021-04-21 14:34:17 -0300

    updated printing

--- cpp/printing.cpp
+++ cpp/printing.cpp
@@ -30,5 +30,18 @@ int main(){
     //by using only one print statement
     cout<<"Hi, im fabio\nim 26\nand love mathmatics!!\n";
 
+    cout<<"-----------------------\n";
+
+    //we can also print numbers and perform arithmetic
+    cout<<10<<"\n"; //10
+    cout<<"10+10+5 = "<<10+10+5<<"\n";//25
+    cout<<"12/2 = "<<12/2<<"\n";//6
+    cout<<"7/2 = "<<7/2<<"\n";     //3  - div w/ 2 integer will provide a integer
+    cout<<"7.0/2 = "<<7.0/2<<"\n"; //3.5  - if we provide at least 1 float/double value, it will provide a double
+    cout<<"10^9 = "<<10*10*10*10*10*10*10*10*10<<"\n"; //1000000000
+    cout<<"10^10 = "<<10*10*10*10*10*10*10*10*10*10<<"\n"; //1410065408  - it will overflow
+
+
+
     return 0;
 }
