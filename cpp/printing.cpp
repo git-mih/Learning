3d2f08a881c245fba834b8a0e643f7e7f8c09364
git-mih <git.mih@gmail.com>
2021-04-21 14:06:43 -0300

    added printing

---
+++ cpp/printing.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //spell cout<<"text" together.
+    cout<<"It is good practice\n";
+
+    cout<<endl; //new line
+
+    //We can have several print commands
+    cout<<"Hi, i am "<<"fabio"<<endl<<"im 26\n";
+    cout<<"and love mathmatics!!";
+    cout<<endl;
+
+    cout<<endl;
+
+    //all of the above code in the same line with 6 print statements
+    cout<<"Hi, im"<<"fabio"<<endl<<"im 26\n"<<"and love mathmatics!!"<<endl;
+    
+    cout<<endl;
+
+    //more readable format
+    cout<<"Hi, im fabio"
+        <<endl<<"im 26"
+        <<endl<<"and love mathmatics!!"
+        <<endl;
+
+    cout<<endl;
+
+    //by using only one print statement
+    cout<<"Hi, im fabio\nim 26\nand love mathmatics!!\n";
+
+    return 0;
+}
