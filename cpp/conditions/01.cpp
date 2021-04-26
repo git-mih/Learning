989d9a8026878252e6b6589077b43f3f825dbd2b
git-mih <82963018+git-mih@users.noreply.github.com>
2021-04-26 18:57:55 -0300

    Update 01.cpp

--- cpp/conditions/01.cpp
+++ cpp/conditions/01.cpp
@@ -2,16 +2,20 @@
 using namespace std;
 
 int main() {
+    // program which get 2 values and perform some operation based on input
+    int a, b; // 1  3
+    char op;  // '+'
+    cout<<"Enter two numbers and a operation: "; // 1 + 3
+    cin>>a>>op>>b;
 
-    int a, b;
-    char op;
-    cout<<"Enter two numbers and a operation.: ";
-    cin>>a>>op>>b; // 1 + 1
-
-    if(op == '+') cout<<a + b<<"\n";
-    if(op == '-') cout<<a - b<<"\n";
-    if(op == '*') cout<<a + b<<"\n";
-    if(op == '/') cout<<(double)a / (double)b<<"\n";
+    if(op == '+') 
+        cout<<a + b<<"\n"; // 4
+    if(op == '-') 
+        cout<<a - b<<"\n"; 
+    if(op == '*') 
+        cout<<a + b<<"\n";
+    if(op == '/') 
+        cout<<(double)a / (double)b<<"\n";
 
     return 0;
 }
