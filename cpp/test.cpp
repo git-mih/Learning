af3211d710ab7da711b6c9c33d15745392e644f3
git-mih <git.mih@gmail.com>
2021-04-28 23:51:26 -0300

    updated a test file

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int main(){
 
-    int n;
-    cin>>n;
+    //program which will count the number of digits of a given integer
+    int num;
+    cin>>num; // 1234
 
-    int i = 0;
-    while(i < n){
-        i++;
+    int digits = 0;
+
+    while(num > 0){ // (1234 > 0) (123 > 0) (12 > 0) (1 > 0) (0 > 0)
+        digits++;   //     0          1         2        3       
+        num /= 10;  // 1234 will became 123, 12, 1 and finally 0
     }
 
-    cout<<i<<"\n";
+    cout<<digits<<"\n";
 
     return 0;
 }
