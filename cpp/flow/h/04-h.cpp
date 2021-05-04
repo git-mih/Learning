5db42977fe0c8d61c11b35a0c9830b4fe48d36c0
git-mih <git.mih@gmail.com>
2021-05-04 01:12:20 -0300

    added new *-h files

--- cpp/flow/h/04-h.cpp
+++ cpp/flow/h/04-h.cpp
@@ -10,11 +10,37 @@ int main(){
     //    output: 62 288                       as 5 + 7*7 + 2*2*2
     //                                            1 + 2*2 + 3*3*3 + 4*4*4*4 
 
+	/*
+	   We need 3 nested loops
+	   loop over test cases
+	   	 loop over reading numbers
+	   	   loop to repeat the number K times (multiplication)
+	*/
+	int T;
+	cin >> T; // 2
 
-    int T = 1;
+	while (T > 0) { // (2 > 0)
+		int N;
+		cin >> N;   // 3
 
-    while (T) {    // 1
-        T--;
-    }
+		int cnt_N = 1;
+		int sum = 0;
+		while (cnt_N <= N) { //       (1 <= 3)         (2 <= 3)     (3 <= 3)
+			int value = 0; 
+			cin >> value;  //            5                7             2
+
+			int cnt_deep = cnt_N; // (cnt_deep = 1) (cnt_deep = 2)   (cnt_deep = 3)
+			int result = 1;
+
+			while (cnt_deep > 0) //                (1 > 0)            (2 > 0, 1 > 0)             (3 > 0, 2 > 0, 1 > 0)
+				result *= value, cnt_deep--; // result = 1*5   result = 1*7 | result = 7*7    result = 1*2 | 2*2 |  2*2*2                          
+                                       //       cnt_deep = 0       cnt_deep = 1   0
+
+			sum += result; // sum = 0+5   sum = 5 + 49   sum = 5 + 49 + 2
+			cnt_N++;      // cnt_N = 2     cnt_N = 3        cnt_N = 4
+		}
+		cout<<sum<<"\n"; // when T = 2   62 
+		T--;             // when T = 1   288
+	}
     return 0;
 }
