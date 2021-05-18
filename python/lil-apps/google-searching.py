4e5794b9ae9b84bedb262c6d837cf7905e4c7269
git-mih <git.mih@gmail.com>
2021-05-18 19:08:29 -0300

    added new file into lil-apps/

---
+++ python/lil-apps/google-searching.py
@@ -0,0 +1,16 @@
+import sys
+import urllib.parse
+import webbrowser
+
+def main(args):
+    def quote(arg):
+        if ' ' in arg:
+            arg = '"%s"' % arg
+        return urllib.parse.quote_plus(arg)
+
+    query_string = '+'.join(quote(arg) for arg in args)
+    url = urllib.parse.urljoin('https://www.google.com/search', '?q=' + query_string)
+    webbrowser.open(url)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
