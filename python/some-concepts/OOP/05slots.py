4ea0dced6b50e1213d1c485a75838fc06a8ac261
git-mih <git.mih@gmail.com>
2021-07-08 17:03:23 -0300

    slots done

--- python/some-concepts/OOP/05slots.py
+++ python/some-concepts/OOP/05slots.py
@@ -56,9 +56,40 @@ p = Point(0, 0)
 # also can cause difficulties in multiple inheritance
 
 #_____________________________________________________________________________________________
+class Location:
+	__slots__ = 'name', '_longitude', '_latitude'
 
-# Slots have doesnt lock class atributes, we still can create new class attributes.
-# but it will lock the instance attributes.
+	def __init__(self, name, longitude, latitude):
+		self._longitude = longitude
+		self._latitude = latitude
+		self.name = name
+	
+	@property
+	def longitude(self):
+		return self._longitude
+
+	@property
+	def latitude(self):
+		return self._latitude
+
+Location.map_service = 'Google Maps'
+# Slots doesnt lock class atributes, we still can create new class attributes.
+
+l = Location('Mumbai', longitude=19.9768, latitude=72.8777)
+# l.__dict__    AttributeError 'Locations' object has no attribute '__dict__'
+# and the instance no longer have the __dict__ attribute.
+
+# but it will lock the instance attributes. at run-time, we are no longer able to
+# add attributes beside (name, _longitude, _latitude) that was defined in slots.
+
+# l.map_link = 'https://...'  # AttributeError as well
+
+# but we can still playing with these defined attributes
+del l.name
+l.name = 'London'
+l._longitude = 30.1234
+
+
 
 #_____________________________________________________________________________________________
 # Slots and single inheritence
@@ -72,7 +103,7 @@ class Student(Person):
 s = Student()
 
 s.name = 'Alex'
-s.__dict__  # {}  exists, we do have an instance dict
+s.__dict__  # {}  exists, now we also have an instance dict 
 #                 but the name isnt stored in the __dict__ attribute.
 
 # in fact, we could assign new attributes to the instance now. but, these attributes
@@ -82,6 +113,7 @@ s.__dict__  # {'age': 18}
 
 # so, subclasses will use slots from the parents (if present), and will also use an
 # instace dict.
+
 # what if we want our subclass to also just use slots?
 # we just require to specify __slots__ in the subclass, only specifying the additional ones.
 class Person:
@@ -108,7 +140,7 @@ s.__dict__   # {}
 
 #________________________________________________________________________________________________
 # how are slotted attributes different from properties?
-# a slotted attribute isnt stored in an instance dicttionary
+# a slotted attribute isnt stored in an instance dictionary
 # properties are also not in an instance dictionary
 class Useless:
 	@property
@@ -128,8 +160,9 @@ obj.__dict__    # {}
 # we can see we do have an instance dict, but doesnt contain the useless property on it.
 
 # they are not different. in fact, both use data descriptors.
-# in both cases, the attributes are present in the class dictionary but with slots,
-# it essentially create properties (getters, setters, deleters and storage) for us.
+# in both cases, the attributes are present in the class dictionary dir(),
+# but with slots it essentially create properties (getters, setters, deleters 
+# and storage) for us.
 
 # the best of both worlds
 # slots        -> faster attribute access, less memory
@@ -138,12 +171,71 @@ obj.__dict__    # {}
 # we can specify __dict__ as a slot an get both working togheter.
 class Person:
 	__slots__ = 'name', '__dict__'
+
 # now we still can add attributes at run-time, these new attributes will be stored 
 # inside the instance dictionary (__dict__)
 p = Person()
 p.name = 'Fabio' # slot
 p.age = 26       # p.__dict__  {'age': 26}
 
-
-
 #_____________________________________________________________________________________________
+class Person:
+	def __init__(self, name): # self = s
+		self.name = name      # s.name = 'fabio'
+
+class Student(Person):
+	pass
+
+p = Person('john')
+p.__dict__   # {..., 'name': 'john'} 
+
+s = Student('fabio')
+s.__dict__   # {'name': 'fabio'}
+
+class Person:
+	__slots__ = 'name',   # Person.__dict__ no longer avaiable.
+
+	def __init__(self, name):
+		self.name = name
+
+class Student(Person):
+	pass
+
+p = Person('john')
+# p.__dict__   AttributeError 'Person' object has no attribute '__dict__'
+
+s = Student('fabio')
+s.__dict__   # {}
+
+# to make subclasses mimic this behavior we could provide an slot with empty tuple
+class Person:
+	def __init__(self, name): # self = s
+		self.name = name      # s.name = 'fabio'
+
+class Student(Person):
+	__slots__ = tuple()  # not useful
+
+s = Student('fabio')
+# s.__dict__   AttributeError 'Student' object has no attribute '__dict__'
+
+class Person:
+	__slots__ = 'name', '__dict__'  # we will be able to add new attributes at run-time
+
+	def __init__(self, name):
+		self.name = name
+
+p = Person('john')
+p.__dict__  # {}
+p.age = 76
+p.__dict__  # {'age': 76}
+
+# another way
+class Person:
+	__slots__ = 'name', '__dict__'
+
+	def __init__(self, name, age):
+		self.name = name
+		self.age = age   # age isnt defined on slots. it is going to be added to __dict__
+
+p = Person('fabio', 26)
+p.__dict__    # {'age': 26}
