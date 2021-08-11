6238733fc73411aab5d9904b77fd08f3e930be5f
git-mih <git.mih@gmail.com>
2021-08-11 15:29:02 -0300

    add metaprogramming/05-06.py files

--- python/some-concepts/OOP/metaprogramming/05metaclasses.py
+++ python/some-concepts/OOP/metaprogramming/05metaclasses.py
@@ -15,18 +15,17 @@ MyType('Person', (), {})    # <class '__main__.Person'>
 # class namespace (dictionary), we had to execute the code within that dictionary, we had to
 # determine the bases, and later on call MyType.
 
-
-
+#_________________________________________________________________________________________________
 # creating classes normally:
-# when we create classes normally we usually use the class keyword only. we dont require to
-# call the type() to do it:
+# when we create classes normally, we usually use the class keyword to do it. we dont require to
+# explicitly call type to create the class object:
 class Person():
     pass
 class Student(Person):
     pass
 
-# Python somehow takes care of all that manual steps we were doing. Python itself is 
-# responsible to call the type and pass the class name, base, dict, etc.
+# Python somehow takes care of all that manual steps we were doing. Python itself is responsible 
+# to call the type and pass the class name, bases, dict, etc.
 
 
 # but what if we have an custom type class for exemple:
@@ -35,14 +34,14 @@ class MyType(type):
         # do stuffs...
         pass
 
-# and we want Python to do that automatic proccess after encounter the `class` keyword but 
-# we want Python to call our custom type to create the class object instead type, 
-# what could we do?
+# and we want Python to do that automatic proccess after compile the `class` keyword but we want
+# Python to call our custom type to create the class object instead type, what could we do?
 
 # this has a technical name, its called Metaclass. 
+
 # for exemple, when we create the class Student, by default its metaclass is type.
-# but instead, we just need to tell Python to use our custom type to create the type instance. 
-# we dont want Python to use the default type class to do it.
+# but instead, we just have to tell Python to use our custom type to create the type instance. 
+# we dont want Python to use the default type class to do the type instance creation.
 
 #___________________________________________________________________________________________________
 # Metaclass:
@@ -58,6 +57,7 @@ class Person(metaclass=MyType):
 
 # when we do that, Python will know it should use our custom type (MyType) to creates the 
 # Person class object instead of using the default type class to do that.
+
 
 # Python will do all that work that we were doing, the only thing now is that, instead of 
 # calling type to create an instance of type (class object), it will call our custom type to
@@ -107,9 +107,52 @@ Person  # <class '__main__.Person'>
 # its basicly the same thing that we have been doing, but now we have Python doing all the
 # hard work for us.
 
+
+
 #______________________________________________________________________________________________
+# we saw that we can create classes ourselves by using type to basicly create instances of
+# type, but that was a quite tedious by having to compile the code and doing all that stuffs.
+
+# but when we use the class keyword, Python does all that stuffs for us. so whenever we
+# executes this code:
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+# now we have a Person class object:
+Person        # <class '__main__.Person'> 
+
+# its type is type itself. that means that Python used the type class to create this class:
+type(Person)  # <class 'type'>
 
 
+# the question is why Python called type to create these instances? 
+# type class is the default metaclass defined in Python. essentially Python is doing it:
+class Person(metaclass=type):
+    def __init__(self, name):
+        self.name = name
+
+Person        # <class '__main__.Person'> 
+type(Person)  # <class 'type'>
 
 
+# but we can specify which metaclass we want to create class objects of:
+class CustomType(type):
+    def __new__(mcls, name, bases, class_dict):
+        print(f'using the metaclass {mcls} to create the class {name}...')
+        class_instance = super().__new__(mcls, name, bases, class_dict)
+        class_instance.get_name = lambda self: self.name
+        return class_instance
 
+# now we can create our class Person but we gonna use the our custom type (metaclass) to create
+# the Person class object:
+class Person(metaclass=CustomType):
+    def __init__(self, name):
+        self.name = name
+
+# when Python reaches and compile the `class` keyword, it will call our metaclass to create 
+# that Person class object:
+#   using the metaclass <class '__main__.CustomType'> to create the class Person...
+
+p = Person('Fabio')
+p.get_name() # Fabio
