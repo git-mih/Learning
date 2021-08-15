f4292b330db86bbcae257df1c3bbc2c735793593
git-mih <git.mih@gmail.com>
2021-08-14 23:19:25 -0300

    add metaprogramming/08.py file

---
+++ python/some-concepts/OOP/metaprogramming/08metaclass_parameters.py
@@ -0,0 +1,81 @@
+# Metaclass parameters
+
+# we saw how regular metaclasses are used, by creating a metaclass by inheriting from type and
+# then we have the __new__ method where Python will pass in the metaclass, name, bases, cls_dict
+# arguments automatically for us, then we do whatever we want and return that new class object:
+class MetaClass(type):
+    def __new__(mcls, name, bases, class_dict):
+        return super().__new__(mcls, name, bases, class_dict) # <'.MetaClass' object at 0x0001>
+
+# then we use it by specificing the metaclass keyword argument, like:
+class MyClass(metaclass=MetaClass):
+    pass
+
+
+# so the question is, can we pass extra parameters to that __new__ method? how we can do that?
+
+#____________________________________________________________________________________________________
+# we just require to add any number of additional parameters inside the __new__ method:
+class MetaClass(type):
+    def __new__(mcls, name, bases, class_dict, arg1, arg2, arg3=None):
+        return super().__new__(mcls, name, bases, class_dict) 
+
+
+# now we need to specify these extra arguments when we define the custom class. but, these
+# extra arguments must be assed as named arguments only:
+class MyClass(metaclass=MetaClass, arg1=10, arg2=20, arg3=30):
+    pass
+
+# we require to pass named arguments cause, the positional arguments are intended to be the 
+# classes that we are inheriting from. for exemple:
+class MyClass(object, metaclass=MetaClass, arg1=10, arg2=20):
+    pass
+
+# notice that, we require to use the keyword argument `metaclass` as well, and any number of
+# arguments that we pass in after it, is required to be keyword argument as well.
+
+# we could also specify these extra arguments before the `metaclass` argument as well. we just
+# require to use named arguments to differentiate them from positional arguments that are ment
+# to be the classes that we want to inherit from:
+class MyClass(object, arg1=10, arg2=20, arg3=30, metaclass=MetaClass):
+    pass
+
+
+#____________________________________________________________________________________________________
+class AutoClassAttrib(type):
+    # lets store these extra attributes inside a tuple, where the first value of the tuple is 
+    # going to be the attribute name, and the second value will be the value itself.
+    def __new__(mcls, name, bases, class_dict, extra_args=None):
+        if extra_args:
+            print(f'creating {name} class with extra attributes:', extra_args)
+            for attr_name, value in extra_args: 
+                class_dict[attr_name] = value
+        return super().__new__(mcls, name, bases, class_dict)
+    # we injected those extra arguments before the new class object gets created. we are
+    # injecting it inside the class_dict (class namespace) essentially.
+        
+class Account(metaclass=AutoClassAttrib, extra_args=(('account_type', 'savings'), ('apr', 0.5))):
+    pass
+
+# when Account class object gets created:
+# creating Account class with extra attributes: (('account_type', 'savings'), ('apr', 0.5))
+
+Account.__dict__  # {'account_type': 'savings', 'apr': 0.5, ...}
+
+
+# we could also inject these extra arguments after we create the class object:
+class AutoClassAttrib(type):
+    def __new__(mcls, name, bases, class_dict, extra_args=None):
+        class_instance = super().__new__(mcls, name, bases, class_dict)
+        if extra_args:
+            print(f'creating {name} class with extra attributes:', extra_args)
+            for attr_name, value in extra_args: 
+                setattr(class_instance, attr_name, value)
+        return class_instance
+        
+class Account(metaclass=AutoClassAttrib, extra_args=(('account_type', 'savings'), ('apr', 0.5))):
+    pass
+# creating Account class with extra attributes: (('account_type', 'savings'), ('apr', 0.5))
+
+vars(Account)    # {..., 'account_type': 'savings', 'apr': 0.5}
+
